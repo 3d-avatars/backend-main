f29d6941902f78361c8bc738307951879286da90
LostImagin4tion <danilov6083@outlook.com>
2024-12-07 14:31:43 +0300

    BACKEND-2: fixed queue repository

---
+++ src/data/repositories/queue/queue_repository_impl.py
@@ -0,0 +1,23 @@
+from aio_pika import Message
+
+from config import get_settings
+from src.data.queue import AMQPChannelManager
+from src.data.repositories.queue import QueueRepository
+
+
+class QueueRepositoryImpl(QueueRepository):
+    def __init__(self):
+        self._channel_manager = AMQPChannelManager()
+
+    async def push_message(self, message_body: str | bytes):
+        if isinstance(message_body, str):
+            message_body = message_body.encode('utf-8')
+
+        async with self._channel_manager.get_channel() as push_channel:
+            await push_channel.default_exchange.publish(
+                message=Message(
+                    body=message_body,
+                ),
+                routing_key=get_settings().RABBITMQ_TASKS_QUEUE,
+            )
+
