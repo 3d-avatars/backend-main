3450a69df1ec811fe39a77bf6dc076ce565b5cf2
LostImagin4tion <danilov6083@outlook.com>
2025-05-10 03:38:14 +0300

    BACKEND-9: fixed creation of redundant db sessions and rabbitmq connections

--- src/data/message_broker/amqp_channel_manager.py
+++ src/data/message_broker/amqp_channel_manager.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 from contextlib import asynccontextmanager
 from typing import Optional
 
@@ -7,50 +9,71 @@ from aio_pika.abc import AbstractRobustConnection
 
 from config import get_settings
 
+logger = logging.getLogger(__name__)
+
 
 class AMQPChannelManager:
 
     def __init__(self) -> None:
-        self.connect_refresh: bool = True
         self._connection: Optional[AbstractRobustConnection] = None
+        self._lock = asyncio.Lock()
+        self._channel: Optional[AbstractRobustChannel] = None
 
     def __new__(cls):
         if not hasattr(cls, "instance"):
             cls.instance = super(AMQPChannelManager, cls).__new__(cls)
-            cls.instance.connect_refresh = True
-
-        return cls.instance  # noqa
+        return cls.instance # noqa
 
     @classmethod
     async def dispose(cls) -> None:
-        if not hasattr(cls, 'instance'):
+        if not hasattr(cls, "instance"):
             return
 
-        await cls.instance._connection.close()
-        delattr(cls.instance)
+        instance = cls.instance # noqa
+
+        if instance._connection and not instance._connection.is_closed:
+            await instance._connection.close()
+            instance._connection = None
+            instance._channel = None
 
     @asynccontextmanager
     async def get_channel(self) -> AbstractRobustChannel:
-        if self.connect_refresh:
-            await self.refresh()
-            self.connect_refresh = False
+        await self.__ensure_connection()
+        try:
+            yield self._channel
+        except Exception as e:
+            logger.exception(f"Error in RabbitMQ channel manager: {e}")
+            self._connection = None
+            self._channel = None
+            raise
+        finally:
+            logger.info(f"Closing RabbitMQ connection")
+            await self._connection.close()
 
-        channel = await self._connection.channel()
-        yield channel
-        await channel.close()
+    async def __ensure_connection(self) -> None:
+        if self._connection is None or self._connection.is_closed or self._channel is None or self._channel.is_closed:
+            async with self._lock:
+                if self._connection is None or self._connection.is_closed:
+                    settings = get_settings()
 
-    async def refresh(self):
-        settings = get_settings()
+                    if self._connection and not self._connection.is_closed:
+                        await self._connection.close()
 
-        self._connection = await aio_pika.connect_robust(
-            host=settings.RABBITMQ_HOST,
-            port=settings.RABBITMQ_PORT,
-            login=settings.RABBITMQ_DEFAULT_USER,
-            password=settings.RABBITMQ_DEFAULT_PASSWORD,
-        )
+                    logger.info("Creating new RabbitMQ connection")
+                    self._connection = await aio_pika.connect_robust(
+                        host=settings.RABBITMQ_HOST,
+                        port=settings.RABBITMQ_PORT,
+                        login=settings.RABBITMQ_DEFAULT_USER,
+                        password=settings.RABBITMQ_DEFAULT_PASSWORD,
+                        heartbeat=60,
+                    )
 
-        async with self._connection.channel() as declare_channel:
-            await declare_channel.set_qos(prefetch_count=1)
-            await declare_channel.declare_queue(
-                name=settings.RABBITMQ_TASKS_QUEUE,
-            )
+                if self._channel is None or self._channel.is_closed:
+                    logger.info("Creating new RabbitMQ channel")
+                    self._channel = await self._connection.channel()
+                    await self._channel.set_qos(prefetch_count=1)
+
+                    await self._channel.declare_queue(
+                        name=settings.RABBITMQ_TASKS_QUEUE,
+                        durable=True,
+                    )
